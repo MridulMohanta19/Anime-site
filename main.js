b81fc9659df5b2641b81f61fcd13cf980c48bea6
MridulMohanta19 <mm8203@srmist.edu.in>
2023-06-08 00:23:15 +0530

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle('active');
+}
